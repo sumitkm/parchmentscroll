ca44a2b16ffd1e2389348df3ea8c486083a34415
Sumit Kumar Maitra <sumitkm@gmail.com>
2015-11-12 07:03:40 +0000

    Updated Blog Page component to show Editor correctly

--- www/app/boot/config.ts
+++ www/app/boot/config.ts
@@ -1,6 +1,7 @@
 
 define(["knockout"],(ko, require) =>
 {
+  ko.components.register("blog-page", { require : "app/ui/pages/blog-page/blog-page" });
   ko.components.register("quill-editor", { require : "app/ui/components/quill-editor/quill-editor" });
   ko.applyBindings();
 });
--- www/app/ui/pages/blog-page/blog-page.ts
+++ www/app/ui/pages/blog-page/blog-page.ts
@@ -1,8 +1,10 @@
+/// <amd-dependency path="text!./blog-page.html" />
 
 var ko = require("knockout");
+export var template = require("text!./blog-page.html");
 
 export class blogPageViewModel
-{  
+{
   constructor(params)
   {
 
