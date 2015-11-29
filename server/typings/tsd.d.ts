bc7030f120dc4427ae9db2f56b09fec64473eb4b
Sumit Kumar Maitra <sumitkm@gmail.com>
2015-11-29 23:50:07 +0000

    Updated tsd.json for server

--- server/typings/tsd.d.ts
+++ server/typings/tsd.d.ts
@@ -1,1 +1,10 @@
 
+/// <reference path="node-azure/azure.d.ts" />
+/// <reference path="crossroads/crossroads.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="js-signals/js-signals.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="requirejs/require.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
